27b573dcee5d4fad3f8af31fbcb8889b0d984afa
Trienlu <biu.lu1122@gmail.com>
2022-02-24 08:23:31 +0700

    Nav-head-TopicModal

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,80 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="header">
+    <div class="header-user">
+      <img src="./assets/pic/users.png" />
+    </div>
+  </div>
+  <div class="nav">
+    <ul class="nav-items">
+      <li class="nav-item" @click="routeAbout">
+        <span class="tooltiptext">Home</span>
+        <img src="./assets/pic/House.png" />
+      </li>
+      <li class="nav-item">
+        <span class="tooltiptext">Bag</span>
+        <img src="./assets/pic/officebag.png" />
+      </li>
+      <li
+        v-if="this.$route.name == 'about'"
+        class="nav-item js-add"
+        @click="modalAct()"
+      >
+        <span class="tooltiptext">Add</span>
+        <img src="./assets/pic/Plus.png" />
+      </li>
+      <li class="nav-item">
+        <span class="tooltiptext">Graph</span>
+        <img src="./assets/pic/bargraph.png" />
+      </li>
+      <li class="nav-item">
+        <span class="tooltiptext">Setting</span>
+        <img src="./assets/pic/Setting.png" />
+      </li>
+    </ul>
+  </div>
+  <div class="router-view">
+    <router-view />
+  </div>
+
+  <Modal-Form @close="modalAct()" :modalActive="modalActive" />
 </template>
+
+<script>
+import ModalForm from "@/components/ModalForm.vue";
+import { ref } from "vue";
+
+export default {
+  components: {
+    ModalForm,
+  },
+  data() {
+    return {};
+  },
+
+  methods: {
+    routeAbout() {
+      this.$router.push({ name: "about" });
+    },
+  },
+
+  setup() {
+    const modalActive = ref(false);
+
+    const modalAct = () => {
+      modalActive.value = !modalActive.value;
+    };
+
+    return { modalActive, modalAct };
+  },
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  height: 1000px;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "./assets/style/main.css";
 </style>
---
+++ src/components/ModalForm.vue
@@ -0,0 +1,77 @@
+<template>
+  <transition>
+    <div class="modal" v-if="modalActive">
+      <span class="modal-backdrop" @click="close"></span>
+      <div class="modal-container">
+        <div class="modal-close" @click="close">
+          <p>X</p>
+        </div>
+        <header class="modal-header">
+          <p>Create New Topic</p>
+        </header>
+        <div class="modal-body">
+          <label for="topic-name" class="modal-label"> Topic's Name </label>
+          <input
+            type="text"
+            class="modal-input"
+            maxlength="35"
+            placeholder="Topic's Name"
+          />
+
+          <label for="topic-type" class="modal-label"> Topic's Type </label>
+          <select class="modal-input">
+            <option value="" selected>Type</option>
+          </select>
+
+          <label for="closuredate" class="modal-label"> Closure date </label>
+          <input type="date" class="modal-input" />
+
+          <label for="color" class="modal-label"> Color </label>
+          <div class="modal-color">
+            <div v-for="color in colors" :key="color">
+              <span
+                class="inner-circle"
+                v-bind:style="{ backgroundColor: color }"
+                @click="checkColor(color)"
+              >
+                <span
+                  v-if="Colorcheck === color"
+                  style="color: white; font-weight: 700"
+                  >&#10003;</span
+                >
+              </span>
+            </div>
+          </div>
+
+          <button class="submit-add">Submit</button>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "ModalForm",
+  data() {
+    return {
+      colors: ["red", "purple", "blue"],
+      Colorcheck: "",
+    };
+  },
+
+  props: ["modalActive"],
+  setup(props, { emit }) {
+    const close = () => {
+      emit("close");
+    };
+
+    return { close };
+  },
+  methods: {
+    checkColor(color) {
+      this.Colorcheck = color;
+    },
+  },
+};
+</script>
