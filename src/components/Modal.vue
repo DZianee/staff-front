f5f3c6deced0d3adf05dee0486eadddbb0ccc9b6
dennydo178 <dovanthanhhuy.94@gmail.com>
2022-03-02 23:20:07 +0700

    add confirm change pass

---
+++ src/components/Modal.vue
@@ -0,0 +1,34 @@
+<template>
+  <transition>
+    <div class="Topic-Modal" v-if="modalActive">
+      <span class="Topic-Modal-backdrop" @click="close"></span>
+      <div class="Topic-Modal-container">
+        <div class="Topic-Modal-close" @click="close">
+          <i class="bi form-control-feedback bi-x-lg" style="font-size: 22px"></i>
+        </div>
+        <header class="Topic-Modal-header">
+          <p>{{ title }}</p>
+        </header>
+        <div class="Topic-Modal-body">
+          <slot></slot>
+          <button class="submit-add" @click="submit">{{ confirmText }}</button>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "TopicModalForm",
+  props: ["modalActive", "title", "confirmText"],
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+    submit() {
+      this.$emit("submit");
+    },
+  },
+};
+</script>
