57e3d12d2841fd17a1e82c2d6d8536a821bb013b
DZianee <hongchauly2001@gmail.com>
2022-03-14 19:06:00 +0700

    fix profile's modal

--- src/components/ProfileAccountInfo.vue
+++ src/components/ProfileAccountInfo.vue
@@ -10,11 +10,20 @@
             <input type="text" :value="accountInfo.username" disabled />
           </div>
           <div class="password">
-            <div class="btn" data-bs-toggle="modal" data-bs-target="#changePassModal">
+            <button class="btn" data-bs-toggle="modal" data-bs-target="#exampleModalLabel">
               <ul>
                 <li><i class="bx-fw bx bx-lock bx-sm bx-tada-hover" /></li>
                 <li>Change password</li>
               </ul>
+            </button>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch demo modal</button>
+            <a href="#" data-reveal-id="myModal">Click Me For A Modal</a>
+
+            <div id="myModal" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+              <h2 id="modalTitle">Awesome. I have it.</h2>
+              <p class="lead">Your couch. It is mine.</p>
+              <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
+              <a class="close-reveal-modal" aria-label="Close">&#215;</a>
             </div>
           </div>
         </div>
