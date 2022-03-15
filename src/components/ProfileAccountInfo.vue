259d30962a70ea4a78699c24c1b7d6384126942b
DZianee <hongchauly2001@gmail.com>
2022-03-15 10:21:29 +0700

    update

--- src/components/ProfileAccountInfo.vue
+++ src/components/ProfileAccountInfo.vue
@@ -16,15 +16,6 @@
                 <li>Change password</li>
               </ul>
             </button>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch demo modal</button>
-            <a href="#" data-reveal-id="myModal">Click Me For A Modal</a>
-
-            <div id="myModal" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
-              <h2 id="modalTitle">Awesome. I have it.</h2>
-              <p class="lead">Your couch. It is mine.</p>
-              <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
-              <a class="close-reveal-modal" aria-label="Close">&#215;</a>
-            </div>
           </div>
         </div>
       </div>
