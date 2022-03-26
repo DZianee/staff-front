667e784b869ba5f248a9ec365a9594e97962446d
DZianee <hongchauly2001@gmail.com>
2022-03-26 23:27:00 +0700

    add topic details page

--- src/views/TopicListDetails.vue
+++ src/views/TopicListDetails.vue
@@ -1,11 +1,234 @@
 <template>
-  <div class="topic-list-details container">this is topic list details pages</div>
+  <div class="topic-list-details container">
+    <div class="topic-details container">
+      <div class="topic-detail-header">
+        <div class="detail-title">
+          <h1>{{ topicDetail.name }}</h1>
+          <p class="create-datetime">Published on {{ getStartDate(topicDetail.startDate) }}</p>
+        </div>
+      </div>
+      <div class="detail-content-wrap">
+        <div class="topic-detail-des">
+          <div class="detail-img">
+            <img :src="`https://${topicDetail.image}`" alt="detail image" />
+          </div>
+          <div class="detail-des" v-html="topicDetail.description"></div>
+        </div>
+        <div class="other-topic-list">
+          <div class="other-topic-header"><h2>Other Topics</h2></div>
+          <div class="other-topic-wrap container">
+            <div class="topic-item" v-for="item in topicList" :key="item.index">
+              <div class="item-img">
+                <img :src="`https://${item.topicImage}`" alt="item image" />
+                <!-- <img
+                  src="https://cdn.dribbble.com/users/823181/screenshots/14754851/media/b4f38450eb494e4e37beb8b206c7f9c0.png?compress=1&resize=1200x900&vertical=top"
+                  alt="item image" /> -->
+              </div>
+              <div class="item-des">
+                <p @click="openTopic(item.id)">{{ item.name }}</p>
+                <p class="time">{{ item.totalIdea }} ideas</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "TopicListDetails",
+  props: {
+    topicId: String,
+  },
+  data() {
+    return {
+      topicDetail: {},
+      id: this.$route.params.id,
+      topicList: [],
+    };
+  },
+  async created() {
+    try {
+      const getTopicList = await this.$axios.post(`api/v1/Topic/GetList`, { searchName: "" }, this.$axios.defaults.headers["Authorization"]);
+      this.topicList = getTopicList.data.content;
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  methods: {
+    getStartDate(date) {
+      const dateCreate = new Date(date);
+      const dmy = dateCreate.toLocaleString();
+      return dmy;
+    },
+    openTopic(id) {
+      this.id = id;
+    },
+    async getTopicDetails() {
+      try {
+        const getTopicDetail = await this.$axios.get(`api/v1/Topic/${this.id}`, this.$axios.defaults.headers["Authorization"]);
+        this.topicDetail = getTopicDetail.data.content;
+        console.log(this.topicDetail);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+  watch: {
+    id() {
+      this.getTopicDetails();
+    },
+  },
+  mounted() {
+    this.getTopicDetails();
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.topic-list-details {
+  padding-top: 40px;
+  right: -8.1%;
+  width: calc(100% - 137px);
+}
+.detail-title .create-datetime {
+  font-size: 16px;
+  padding-top: 10px;
+}
+.detail-content-wrap {
+  margin-top: 2%;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  column-gap: 40px;
+}
+img {
+  height: max(556px);
+  width: max(825px);
+    object-fit: cover;
+
+}
+.detail-des {
+  padding: 20px;
+  line-height: 40px;
+  font-size: 20px;
+  text-align: justify;
+}
+/* p {
+  font-size: 18px;
+} */
+.other-topic-header h2 {
+  padding: 10px;
+  font-size: 24px;
+}
+.other-topic-list {
+  border-left: 1px solid rgb(179, 179, 179);
+}
+.other-topic-wrap {
+  display: grid;
+  row-gap: 40px;
+  height: 900px;
+  overflow: hidden;
+  overflow-y: auto;
+}
+.other-topic-wrap .topic-item {
+  margin-top: 2%;
+  display: grid;
+  grid-template-columns: 1.2fr 2fr;
+  column-gap: 10px;
+}
+.item-img img {
+  width: max(88%);
+  height: max(110px);
+    object-fit: cover;
+
+}
+.item-des p:not(.time) {
+  font-size: 18px;
+  color: black;
+  font-weight: 500;
+}
+.item-des p:not(.time):hover {
+  cursor: pointer;
+  color: #3d5afe;
+  text-decoration: underline;
+}
+@media screen and (max-width: 1440px) {
+  .topic-list-details {
+    width: calc(100% - 272px);
+  }
+}
+@media screen and (max-width: 1025px) {
+  .topic-list-details {
+    width: calc(100% - 101px);
+    right: -4%;
+  }
+  img {
+    height: max(380px);
+  }
+  .item-img img {
+    width: max(100%);
+    height: max(90px);
+  }
+}
+@media screen and (max-width: 768px) {
+  .topic-list-details {
+    width: calc(100% - 92px);
+    right: -5%;
+  }
+  .detail-content-wrap {
+    margin-top: 2%;
+    display: grid;
+    grid-template-columns: 100%;
+  }
+  /* img {
+  height: max(36px);
+  width: max(825px);
+} */
+  .other-topic-list {
+    border-left: none;
+  }
+  .other-topic-wrap {
+    row-gap: 30px;
+    height: 400px;
+  }
+  .item-des p:not(.time) {
+    font-size: 19px;
+  }
+  .item-des p:not(:nth-child(1)) {
+    font-size: 16px;
+  }
+  .detail-des {
+  font-size: 18px;
+}
+.item-img img {
+    width: max(90%);
+    height: max(100px);
+  }
+}
+@media screen and (max-width: 480px) {
+  .topic-list-details {
+    width: 100%;
+    right: 0;
+    top: 70px;
+  }
+  .topic-detail-header .detail-title h1 {
+    font-size: 24px;
+    line-height: 30px;
+  }
+  .detail-title .create-datetime {
+    font-size: 15px;
+  }
+  .other-topic-wrap {
+    row-gap: 30px;
+    height: 350px;
+  }
+  .other-topic-list {
+    height: 480px;
+  }
+  .detail-des {
+    font-size: 16px;
+  }
+}
+</style>
--- src/views/TopicListView.vue
+++ src/views/TopicListView.vue
@@ -4,13 +4,11 @@
       <div class="lastest-topic-header"><h2>Lastest Topic</h2></div>
       <div class="lastest-topic-wrap">
         <div class="lastest-topic-content">
-          <h2 class="title">{{ lastestTopic.name }}</h2>
+          <h2 class="title" @click="TopicDetailsRoute(lastestTopic.id)">{{ lastestTopic.name }}</h2>
           <p>{{ getStartDate }} - {{ getEndDate }}</p>
         </div>
         <div class="lastest-topic-img">
-          <img
-            src="https://cdn.dribbble.com/users/823181/screenshots/16719702/media/b986bfb7f5cae8342bb9e89aaf17bdcf.png?compress=1&resize=1200x900&vertical=top"
-            alt="Topic lastest img" />
+          <img :src="`https://${lastestTopic.image}`" alt="Topic lastest img" />
         </div>
       </div>
     </section>
@@ -19,12 +17,10 @@
       <div class="other-topic-wrap container">
         <div class="topic-item" v-for="item in topicList" :key="item.index">
           <div class="item-img">
-            <img
-              src="https://cdn.dribbble.com/users/823181/screenshots/14754851/media/b4f38450eb494e4e37beb8b206c7f9c0.png?compress=1&resize=1200x900&vertical=top"
-              alt="item image" />
+            <img :src="`https://${item.topicImage}`" alt="item image" />
           </div>
           <div class="item-des">
-            <p>{{ item.name }}</p>
+            <p @click="TopicDetailsRoute(item.id)">{{ item.name }}</p>
             <p class="time">{{ item.totalIdea }} ideas</p>
           </div>
         </div>
@@ -72,6 +68,11 @@ export default {
       return dm;
     },
   },
+  methods: {
+    TopicDetailsRoute(id) {
+      this.$router.push({ name: "topicListView", params: { id: id } });
+    },
+  },
 };
 </script>
 
@@ -88,7 +89,7 @@ export default {
   border-bottom: solid 1px rgb(129, 128, 128);
   margin-bottom: 2%;
   letter-spacing: 0.5px;
-  background: linear-gradient(to left, #FBF8F1, #649866);
+  background: linear-gradient(to left, #fbf8f1, #649866);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
@@ -115,6 +116,10 @@ export default {
 .lastest-topic-content p {
   font-size: 22px;
 }
+img {
+  height: max(495px);
+  width: max(660px);
+}
 .other-topics {
   margin-top: 5%;
 }
@@ -122,7 +127,7 @@ export default {
   text-align: center;
   border-bottom: solid 1px rgb(129, 123, 123);
   letter-spacing: 0.8px;
-  background: linear-gradient(to left, #FBF8F1, #649866);
+  background: linear-gradient(to left, #fbf8f1, #649866);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
@@ -138,8 +143,9 @@ export default {
   row-gap: 40px;
   padding-left: 4.2%;
 }
-.item-img {
-  width: 88%;
+.item-img img {
+  width: max(88%);
+  height: max(220px);
 }
 .item-des {
   text-align: left;
@@ -193,6 +199,10 @@ export default {
   .lastest-topic-content p {
     font-size: 18px;
   }
+  img {
+    height: max(300px);
+    width: max(660px);
+  }
   h2 {
     font-size: 25px;
   }
@@ -239,6 +249,10 @@ export default {
   }
   .other-topics .other-topic-header {
     font-size: 22px;
+  }
+  img {
+    height: max(200px);
+    width: max(660px);
   }
 }
 </style>
